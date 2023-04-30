b0459e5d83ba1c2778ecc28c9567a4c60e6592d4
M-ST <M-ST@DESKTOP-LUQCIUM>
2023-04-30 11:39:24 +0430

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("inter a number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+           double xy= metoodMath(x);
+            Console.WriteLine(xy);
+            Console.ReadKey();
+        }
+         static public double metoodMath (int number )
+        {
+            double y = Math.Sqrt(Math.Abs(number) + Math.Sqrt(Math.Abs(number)) + Math.Sqrt(Math.Abs(number)));
+            return y;
+
+        }
+    }
+}
